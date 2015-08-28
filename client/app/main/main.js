725849d5ee8cb5c828cba9f7043c30383a14c8ca
Matt Everson <meverson@tenable.com>
2015-08-27 20:18:56 -0500

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fccNightlifeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
